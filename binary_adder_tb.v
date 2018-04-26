1887c0eaf1116ea046dcdd130a1b52f58af3f181
Syn <syennyw98@gmail.com>
2018-04-26 21:26:15 +0800

    Update binary_adder_tb.v

--- binary_adder_tb.v
+++ binary_adder_tb.v
@@ -9,17 +9,23 @@ wire[5:0] sum;
 binary_adder adder_0 ( .a(a), .b(b), .sum(sum));
 
 initial begin
-		$dumpfile("binary_adder.vcd");
-		$dumpvars;
+	$dumpfile("binary_adder.vcd");
+	$dumpvars;
 end
 
 initial begin
-		a = 6'b000101; b = 6'b000011;		//sum should be 6'b001000
-		# 100 a = 6'b000101; b = 6'b000111;	//sum should be 6'b001100
-		# 100 a = 6'b001001; b = 6'b000010;	//sum should be 6'b001011
-		# 100 a = 6'b011001; b = 6'b100010;	//sum should be 6'b111011
-		# 100;
-		$finish;
+	a = 6'b000101; b = 6'b000011;		//sum should be 6'b001000
+	# 100 a = 6'b000101; b = 6'b000111;	//sum should be 6'b001100
+	# 100 a = 6'b001001; b = 6'b000010;	//sum should be 6'b001011
+	# 100 a = 6'b011001; b = 6'b100010;	//sum should be 6'b111011
+	# 100 a = 6'b001000; b = 6'b111011; 	//sum should b'6000011
+	# 100 a = 6'b111101; b = 6'b111110;	//sum should be 6'b111011
+	# 100;
+	$finish;
+end
+
+initial begin 
+	$monitor(,$time, " a = %b, b = %b, sum = %b\n", a, b, sum);
 end
 
 endmodule
